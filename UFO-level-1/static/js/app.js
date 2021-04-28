4a9b9e3871e6826ccc5e0b37ebce08450f153d81
tnguyen0306 <tnguyen0306@gmail.com>
2021-04-27 22:08:33 -0500

    Beta 1.2.0 - append data to DOM

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,10 +2,10 @@
 var tableData = data;
 
 // Select the button
-var button = d3.select("#button");
+var button = d3.select("#filter-btn");
 
 // Select the form
-var form = d3.select("#form-group");
+var form = d3.select("form");
 
 // Create event handlers 
 button.on("click", runEnter);
@@ -18,15 +18,31 @@ function runEnter() {
   d3.event.preventDefault();
   
   // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#form-control");
+  var inputElement = d3.select("#datetime");
 
   // Get the value property of the input element
   var inputValue = inputElement.property("value");
 
+  // Print the value to the console
   console.log(inputValue);
-  console.log(tableData);
 
-  var filteredData = tableData.filter(event => event.datetime === inputValue);
+  // Filter the data based on input value
+  var filteredData = tableData.filter(dataInfo => dataInfo.datetime === inputValue);
 
+  // Print the value to the console
   console.log(filteredData);
+
+  // Then, select the unordered list element by class name
+  var list = d3.select("tbody");
+
+  // remove any children from the list to
+  list.html("");
+
+  // append stats to the list
+  filteredData.forEach(events => {
+    var row = list.append("tr");
+    var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"];
+    columns.forEach(column => row.append("td").text(events[column])
+    )
+  });
 };
