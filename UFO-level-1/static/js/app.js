6218eba1e4d3df8e7096a1a72e7eee7a03ad7e3a
tnguyen0306 <tnguyen0306@gmail.com>
2021-04-27 21:24:42 -0500

    Beta 1.1.0 - setup filter data

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,32 @@
+// from data.js
+var tableData = data;
+
+// Select the button
+var button = d3.select("#button");
+
+// Select the form
+var form = d3.select("#form-group");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+  
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#form-control");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  console.log(tableData);
+
+  var filteredData = tableData.filter(event => event.datetime === inputValue);
+
+  console.log(filteredData);
+};
